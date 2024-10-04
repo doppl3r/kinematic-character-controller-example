f7a98493dbadbd27bef2ad965fdd0ffed3c2ffab
Jacob DeBenedetto <deben3@gmail.com>
2024-10-04 13:48:12 -0700

    Add tween to library

--- src/js/entities/Entity.js
+++ src/js/entities/Entity.js
@@ -1,5 +1,6 @@
 import { EventDispatcher, Object3D, Quaternion, Vector3 } from 'three';
 import { ActiveCollisionTypes, ActiveEvents, ColliderDesc, RigidBodyDesc, RigidBodyType } from '@dimforge/rapier3d';
+import { Easing, Group, Tween } from '@tweenjs/tween.js'
 
 /*
   An entity is an abstract class that contains a single 3D object and a
@@ -15,7 +16,7 @@ class Entity extends EventDispatcher {
     // Inherit Three.js EventDispatcher system
     super();
 
-    // These components are created when this entity is added to scene/world
+    // Set base components
     this.rigidBody;
     this.rigidBodyDesc;
     this.colliders = new Map();
@@ -27,6 +28,11 @@ class Entity extends EventDispatcher {
       quaternion_1: new Quaternion(),
       quaternion_2: new Quaternion()
     };
+    this.tweens = new Group();
+    this.forceDirection = new Vector3();
+    this.forceAcceleration = 1;
+    this.forceSpeedMax = Infinity;
+    this.parent;
 
     // Define initial rigidBodyDesc and colliderDesc
     this.setRigidBodyDesc(options);
@@ -35,19 +41,23 @@ class Entity extends EventDispatcher {
     // Update 3D object position/rotation
     this.takeSnapshot();
     this.lerp(1);
-
-    // Bind "this" context to class function (required for event removal)
+    
+    // Add entity event listeners
     this.onCollision = this.onCollision.bind(this);
     this.onRemoved = this.onRemoved.bind(this);
-
-    // Add entity event listeners
     this.addEventListener('collision', this.onCollision);
     this.addEventListener('removed', this.onRemoved);
   }
 
   update(delta) {
     // Take a snapshot every time the entity is updated
-    if (this.rigidBody) this.takeSnapshot();
+    this.takeSnapshot();
+
+    // Check local force
+    this.updateForce();
+
+    // Dispatch update event to listeners
+    this.dispatchEvent({ type: 'updated' });
   }
 
   render(delta, alpha) {
@@ -56,6 +66,9 @@ class Entity extends EventDispatcher {
 
     // Interpolate 3D object position
     this.lerp(alpha);
+
+    // Update optional tweens
+    this.tweens.update();
   }
 
   setRigidBodyDesc(options) {
@@ -70,11 +83,11 @@ class Entity extends EventDispatcher {
       position: { x: 0, y: 0, z: 0 },
       quaternion: { x: 0, y: 0, z: 0, w: 1 },
       softCcdPrediction: 0,
-      status: 'Dynamic', // 0: Dynamic, 1: Fixed, 2: KinematicPositionBased, 3: KinematicVelocityBased
+      status: 0, // 0: Dynamic, 1: Fixed, 2: KinematicPositionBased, 3: KinematicVelocityBased
     }, options);
 
     // Initialize rigid body description
-    this.rigidBodyDesc = new RigidBodyDesc(RigidBodyType[options.status]);
+    this.rigidBodyDesc = new RigidBodyDesc(isNaN(options.status) ? RigidBodyType[options.status] : options.status);
     this.rigidBodyDesc.enabledRotations(options.enabledRotations.x, options.enabledRotations.y, options.enabledRotations.z);
     this.rigidBodyDesc.enabledTranslations(options.enabledTranslations.x, options.enabledTranslations.y, options.enabledTranslations.z);
     this.rigidBodyDesc.setAngularDamping(options.angularDamping);
@@ -149,12 +162,44 @@ class Entity extends EventDispatcher {
     }
   }
 
+  getPosition() {
+    if (this.rigidBody == null) return this.object.position;
+    else if (this.rigidBody.isKinematic()) return this.rigidBody.nextTranslation()
+    else return this.rigidBody.translation();
+  }
+
   setPosition(position) {
     if (this.rigidBody) this.rigidBody.setTranslation(position);
   }
 
+  getRotation() {
+    if (this.rigidBody == null) return this.object.quaternion;
+    else if (this.rigidBody.isKinematic()) return this.rigidBody.nextRotation()
+    else return this.rigidBody.rotation();
+  }
+
   setRotation(quaternion) {
     if (this.rigidBody) this.rigidBody.setRotation(quaternion);
+  }
+
+  getScale() {
+    return this.object.scale;
+  }
+
+  setScale(scale) {
+    // Colliders cannot be resized directly 
+    this.object.scale.copy(scale);
+  }
+
+  tween(options) {
+    // Assign easing from string (ex: "Quadratic.InOut") - https://stackoverflow.com/a/43849204/2510368
+    if (typeof options.easing == 'string') {
+      options.easing = options.easing.split('.').reduce((p,c)=>p&&p[c]||null, Easing);
+    }
+
+    // Create and assign tween to tween group
+    var tween = new Tween(options.object, this.tweens).to(options.to, options.duration).dynamic(options.dynamic).easing(options.easing).interpolation(options.interpolation).onStart(options.onStart).onUpdate(options.onUpdate).onComplete(options.onComplete);
+    return tween;
   }
 
   takeSnapshot() {
@@ -205,21 +250,60 @@ class Entity extends EventDispatcher {
     e.target.removeEventListener('removed', e.target.onRemoved);
   }
 
-  applyImpulseFromNormal(_force, _normal) {
-    const force = new Vector3().copy(_force);
-    const normal = new Vector3().copy(_normal).normalize();
-    const velocity = new Vector3().copy(this.rigidBody.linvel());
-    const angle = Math.atan2(normal.y, normal.x) + (Math.PI / 2);
-
-    // Set velocity y-value to zero
-    velocity.applyAxisAngle({ x: 0, y: 0, z: 1 }, angle);
-    velocity.y = 0;
+  applyVelocityAtAngle(force = { x: 1, y: 1, z: 1 }, angle) {
+    // Rotate and apply velocity at an angle
+    var velocity = new Vector3().copy(this.rigidBody.linvel());
     velocity.applyAxisAngle({ x: 0, y: 0, z: 1 }, -angle);
-    this.rigidBody.setLinvel(velocity);
-    
-    // Rotate force by normal angle
+    velocity.multiply(force);
+    velocity.applyAxisAngle({ x: 0, y: 0, z: 1 }, angle);
+    this.rigidBody.setLinvel(velocity, true);
+  }
+
+  setAngularVelocityAtAngle(force = { x: 1, y: 1, z: 1 }, angle) {
+    var velocity = new Vector3().copy(this.rigidBody.linvel());
+    var direction = 1;
+
+    // Rotate velocity before computing direction
+    velocity.applyAxisAngle({ x: 0, y: 0, z: 1 }, -angle);
+    direction *= -Math.sign(Math.round(velocity.x)); // -1, 0, or 1
+    force = new Vector3().copy(force);
+    force.multiplyScalar(direction);
+    this.rigidBody.setAngvel(force, true);
+  }
+
+  applyImpulseAtAngle(force = { x: 0, y: 0, z: 0 }, angle) {
+    // Rotate and apply force at an angle
+    force = new Vector3().copy(force);
     force.applyAxisAngle({ x: 0, y: 0, z: 1 }, angle);
     this.rigidBody.applyImpulse(force, true);
+  }
+
+  setForce(direction = { x: 0, y: 0, z: 0 }, acceleration = 1, max = Infinity) {
+    this.forceDirection.copy(direction).normalize(); // Ex: -1.0 to 1.0
+    this.forceAcceleration = acceleration;
+    this.forceSpeedMax = max;
+  }
+
+  updateForce() {
+    // Check if force exists
+    if (this.forceDirection.length() > 0) {
+      _vector.copy(this.rigidBody.linvel());
+      const speed = _vector.dot(this.forceDirection);
+      const speedNext = speed + this.forceAcceleration; // Ex: 0.5 to 4.5
+      const speedClamped = Math.max(speed, Math.min(speedNext, this.forceSpeedMax));
+      const acceleration = speedClamped - speed; // Ex: 0.5 (or 0 at max speed)
+      
+      // Update velocity using new force
+      _vector.x += this.forceDirection.x * acceleration;
+      _vector.y += this.forceDirection.y * acceleration;
+      _vector.z += this.forceDirection.z * acceleration;
+      this.rigidBody.setLinvel(_vector);
+    }
+  }
+
+  getSpeed() {
+    if (this.rigidBody == null) return 0;
+    return _vector.copy(this.rigidBody.linvel()).length();
   }
 
   toJSON() {
@@ -262,5 +346,8 @@ class Entity extends EventDispatcher {
     }
   }
 }
+
+// Assign local helper components
+let _vector = new Vector3();
 
 export { Entity };
