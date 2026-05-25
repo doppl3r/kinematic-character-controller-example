1fe951a29fd13f2b452170bfb9815ac1cf3accc5
Jacob DeBenedetto <deben3@gmail.com>
2026-05-25 16:31:22 -0700

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   build: {
     emptyOutDir: true,
-    outDir: './build',
-    target: "ES2022"
+    outDir: './build'
   },
   css: {
     preprocessorOptions : {
@@ -19,7 +17,6 @@ export default defineConfig({
     } 
   },
   plugins: [
-    topLevelAwait(),
     vue(),
     wasm()
   ],
